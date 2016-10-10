e963f556c8b9ac9c631cc282a8a695c28660ddbd
unknown <ajitdas2900@gmail.com name = Ajit Das>
2016-10-10 12:24:48 +0600

    array_flip

--- compact.php
+++ compact.php
@@ -10,3 +10,10 @@ $location_vars = array("city", "state");
 $result = compact("event", "nothing_here", $location_vars);
 print_r($result);
 ?>
+<br>
+<?php
+$input = array("oranges", "apples", "pears");
+$flipped = array_flip($input);
+
+print_r($flipped);
+?>
